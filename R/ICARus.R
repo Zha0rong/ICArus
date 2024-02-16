6de865533b7631fd4a6412629d173cc1bcee882e
Zha0rong Li <34935078+Zha0rong@users.noreply.github.com>
2024-02-16 14:20:10 -0500

    Update ICARus.R

--- R/ICARus.R
+++ R/ICARus.R
@@ -58,7 +58,7 @@ ICARus <- function(Matrix,numberofcomponents,iteration=100,numberofcores=2,dista
     
     Disimmilarity.Results=list()
     Group=stringr::str_split_fixed(colnames(Corrected.Signature.Matrix),pattern = '_',n=2)[,1]
-    names(Group)=colnames(Signature.Matrix)
+    names(Group)=colnames(Corrected.Signature.Matrix)
     
     Disimmilarity.Results$Clustering.results.item$clustering=Individual_Clustering(Matrix=Corrected.Signature.Matrix,Group=Group,ncluster=numberofcomponents,method=clustering_algorithm)
     
