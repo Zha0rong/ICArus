e67d59c9416ce5e1f8bbb901da2895d8f9425809
Zha0rong Li <34935078+Zha0rong@users.noreply.github.com>
2024-02-17 22:41:51 -0500

    Update ICARus.R

--- R/ICARus.R
+++ R/ICARus.R
@@ -61,7 +61,7 @@ ICARus <- function(Matrix,numberofcomponents,iteration=100,numberofcores=2,dista
     #names(Group)=colnames(PCA.space)
     #Disimmilarity.Results$Clustering.results.item$clustering=Individual_Clustering(Matrix=PCA.space,Group=Group,ncluster=numberofcomponents,method=clustering_algorithm,distance_measure=distance_measure)
     correlation=as.matrix(parallelDist::parallelDist(t(PCA.space)))
-    Disimilarity.fixed=1-(1/(1+correlation))
+    Disimilarity.fixed=1-abs(1/(1+correlation))
     cluster=hclust(as.dist(correlation),method = clustering_algorithm)
     cluster=cutree(cluster,numberofcomponents)
     Disimmilarity.Results$Clustering.results.item$clustering=cluster
@@ -77,7 +77,7 @@ ICARus <- function(Matrix,numberofcomponents,iteration=100,numberofcores=2,dista
   colnames(Clustered.Signature.matrix)=seq(1,ncol(Clustered.Signature.matrix))
   colnames(Clustered.Signature.matrix)=paste('signature.',colnames(Clustered.Signature.matrix),sep = '')
   colnames(Clustered.Affiliation.matrix)=paste('signature.',colnames(Clustered.Affiliation.matrix),sep = '')
-  a=Cluster_Stability_Calculation(abs(1-(1/(1+correlation))),Clustering_identity = Disimmilarity.Results$Clustering.results.item$clustering,numberofcores = 6)
+  a=Cluster_Stability_Calculation(abs(1/(1+correlation)),Clustering_identity = Disimmilarity.Results$Clustering.results.item$clustering,numberofcores = 6)
   a=data.frame(ICs=rep(numberofcomponents,length(a)),ClusterNumber=names(a),QualityIndex=a)
   b=data.frame(table(Disimmilarity.Results$Clustering.results.item$clustering))
   rownames(b)=b$Var1
