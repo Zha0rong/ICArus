f6e6bbc71dfe5fe5a987f0bf88a0d0e99ceacd0f
Zha0rong Li <34935078+Zha0rong@users.noreply.github.com>
2024-02-28 10:14:24 -0500

    Update ICARus.R

--- R/ICARus.R
+++ R/ICARus.R
@@ -389,8 +389,8 @@ ICARus_complete <- function(Matrix,iteration=100,numberofcores=4,
     }
   }
   if (length(names(Filtered.Results))<(numbers_of_parameter_for_reproducibility_test/2)) {
-    print('There is not enough signatures pass quality index threshold. Please increase the parameter upperbound, lower the parameter lowerbound and lower the parameter quality.index.threshold')
-    exit()
+    exit('There is not enough signatures pass quality index threshold. 
+         Please increase the parameter upperbound, lower the parameter lowerbound and lower the parameter quality.index.threshold')
   }
     
   Clustered.Signature.matrix=list()
