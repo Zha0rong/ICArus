826cecfcda172dec699783193727964c713a7d47
Zha0rong Li <34935078+Zha0rong@users.noreply.github.com>
2024-03-01 10:18:29 -0500

    Add in description for example data

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' @title Count.Matrix
+#'
+#' @description Raw Count Matrix of RNA-Seq samples from published study Rade M, Böhlen S, Neuhaus V, Löffler D et al. A time-resolved meta-analysis of consensus gene expression profiles during human T-cell activation. Genome Biol 2023 Dec 14;24(1):287. PMID: 38098113.
+#'
+#' @format A data frame with 57,133 rows and 44 variables.
+#' @source <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE197067>
+"Count.Matrix"
+
+#' @title metadata
+#'
+#' @description Metadata of RNA-Seq samples from published study Rade M, Böhlen S, Neuhaus V, Löffler D et al. A time-resolved meta-analysis of consensus gene expression profiles during human T-cell activation. Genome Biol 2023 Dec 14;24(1):287. PMID: 38098113.
+#'
+#' @format A data frame with 44 rows and 3 variables:
+#' \describe{
+#'   \item{Replicates}{The replicate identities of the sample.}
+#'   \item{Timepoint}{The time when the sample is obtained.}
+#'   \item{Condition}{The stimulation condition of the sample.}
+#' }
+#' @source <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE197067>
+"metadata"
+
+
