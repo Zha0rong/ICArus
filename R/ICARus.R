6fb352452d6f2f331c6842e04fa2ff011251561d
Zha0rong Li <34935078+Zha0rong@users.noreply.github.com>
2025-01-11 23:03:21 -0500

    Update ICARus.R

--- R/ICARus.R
+++ R/ICARus.R
@@ -304,7 +304,7 @@ ICARus_complete <- function(Matrix,iteration=100,numberofcores=4,
   
   
   Results=list()
-  for (i in seq(optimal,optimal+numbers_of_parameter_for_reproducibility_test)) {
+  for (i in seq(optimal,(optimal+numbers_of_parameter_for_reproducibility_test-1))) {
     ICAResults=ICARus(Matrix = Matrix,numberofcomponents = i,iteration = iteration,numberofcores = numberofcores,clustering_algorithm = clustering_algorithm,distance_measure = distance_measure,Hierarchical.clustering.method = Hierarchical.clustering.method,tol=tolerance,maxit=max.iteration)
     Results[[paste0('IC.',i)]]=ICAResults
     rm(ICAResults)
